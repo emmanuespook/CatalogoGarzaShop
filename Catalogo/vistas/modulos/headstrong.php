f67f5b872dda6d5b0f8ad3cf3908fa162e51fa81
Emmanue Hernandez <emmanuespook@hotmail.com>
2020-02-24 12:51:36 -0600

    Archivos agregados de las vistas modulos

---
+++ Catalogo/vistas/modulos/headstrong.php
@@ -0,0 +1,19 @@
+<header>
+	<nav class="navbar fixed-top navbar-expand-lg navbar-dark transparent">
+	  <a class="navbar-brand" href="#">
+	  	<img src="vistas/img/logo.png" height="60px" class="d-inline-block align-top" alt="">
+	  </a>
+	  <!-- <button class="navbar-toggler" type="button" data-toggle="collapse">  data-target="#ini"
+	    <span class="glyphicon glyphicon-home"></span>
+	  </button> -->
+	  <div class="collapse navbar-collapse" id="ini">
+	    <ul class="nav navbar-nav">
+	      <li class="nav-item active">
+	        <a class="nav-link" href="http://www.unicomercializadora.com/">INICIO</a>
+	      </li>
+	    </ul>
+	  </div>
+	</nav>
+</header>
+
+
---
+++ Catalogo/vistas/modulos/inicio.php
@@ -0,0 +1,70 @@
+<!-- /*******************************************
+Developed By Carlos Emmanue Hernandez Correa
+2020
+Contact
+  emmanuespook@hotmail.com
+********************************************/ -->
+<div class="container catalogo">
+    <div id="magazine">
+      <div id="hard" style="background: black">
+        <div class="container">
+          <img src="vistas/img/Catalogo/garza1.jpg" width="100%" alt="">
+        </div>
+      </div>
+      <div id="hard">
+        <div class="container-fluid">
+          <img src="vistas/img/Catalogo/garza2.jpg" width="100%" alt="">
+        </div>
+      </div>
+
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza3.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza4.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza5.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza6.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza7.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza8.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza9.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza10.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza11.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza12.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza13.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza14.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza15.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza16.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza17.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza18.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza19.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza20.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza21.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza22.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza23.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza24.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza25.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza26.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza27.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza28.jpg);"></div>
+      <div class="turn-page" style="background-image:url(vistas/img/Catalogo/garza29.jpg);"></div>
+
+
+      <div id="hard">
+        <div class="container-fluid">
+          <img src="vistas/img/Catalogo/garza30.jpg" width="100%" alt="">
+        </div>
+      </div>
+
+    </div> 
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm-2">
+    </div>
+    <div class="col-sm-8">
+      <center>
+        <button class="btn btn-success btn-lg btn-ant"> Anterior </button>
+        <button class="btn btn-success btn-lg btn-sig"> Siguiente </button>
+      </center>
+    </div>
+    <div class="col-sm-2">
+    </div>
+  </div>
