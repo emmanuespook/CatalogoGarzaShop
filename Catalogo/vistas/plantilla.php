53fa45d70eaa6f6b1eac157024af7608756c33ca
Emmanue Hernandez <emmanuespook@hotmail.com>
2020-02-24 12:53:05 -0600

    archivo plantilla

---
+++ Catalogo/vistas/plantilla.php
@@ -0,0 +1,50 @@
+<!--
+/*******************************************
+Developed By Carlos Emmanue Hernandez Correa
+2020
+Contact
+  emmanuespook@hotmail.com
+********************************************/
+-->
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" charset="utf-8">
+	<title>
+		Catalogo
+	</title>
+  
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="icon" href="vistas/img/logo.png">
+
+  <!-- ************************* DESIGN CSS ************************* -->
+
+  <link rel="stylesheet" href="vistas/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="vistas/css/catalogo.css">
+
+  <!-- ************************* PUGLIN JS ************************* -->
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="vistas/js/turn.min.js"></script>
+</head>
+
+<body>
+  <?php
+      /*=================================
+                    CABEZA
+      =================================*/
+      include "modulos/headstrong.php";
+      /*=================================
+                  END CABEZA
+      =================================*/
+      include "modulos/inicio.php";
+  ?>
+  
+
+  <script type="text/javascript" src="vistas/js/plantilla.js"></script>
+
+
+</body>
+</html>
